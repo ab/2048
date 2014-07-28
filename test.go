17394f363abb69d50c3b10ed5acca3373ef3035e
Andy Brody <andy@abrody.com>
2014-07-28 00:48:55 -0700

    Make keyboard test a little prettier.

--- test.go
+++ test.go
@@ -11,21 +11,26 @@ func main() {
     if err != nil {
         panic(err)
     }
+    defer fmt.Println("Goodbye!")
     defer termbox.Close()
+
+    fmt.Println("Entered raw keyboard mode. Press any key to see it printed.\r")
+    fmt.Println("Use ctrl+c or ctrl+q to exit.\r")
 
     // termbox.SetInputMode(termbox.InputEsc)
 
     for {
+        fmt.Println("")
         switch ev := termbox.PollEvent(); ev.Type {
         case termbox.EventKey:
-            if ev.Key == termbox.KeyCtrlQ {
-                fmt.Println("Goodbye!")
+            if ev.Key == termbox.KeyCtrlQ || ev.Key == termbox.KeyCtrlC {
+                fmt.Println("Exiting...")
                 return
             }
-            fmt.Printf("EventKey: %#v\n", ev.Key)
+            fmt.Printf("EventKey: %#v\n\r", ev.Key)
             var buf [utf8.UTFMax]byte
             n := utf8.EncodeRune(buf[:], ev.Ch)
-            fmt.Printf("Rune: %s\n", buf[:n])
+            fmt.Printf("Rune[%d]: %s\n\r", n, buf[:n])
         case termbox.EventResize:
             fmt.Println("EventResize")
         case termbox.EventMouse:
